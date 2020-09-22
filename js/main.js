99f81f3f42307debfa0579523fb56f2e9d8f3a85
nightbarron <hoangtuan140982@gmail.com>
2020-09-23 00:19:31 +0700

    v 1.2

--- js/main.js
+++ js/main.js
@@ -17,5 +17,26 @@ $(document).ready(function(){
     })
   });
 
+  //Portfolio
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+  
+
+
 
 
